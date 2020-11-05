727ee5ae844ad67ff175d5713a7050beaa3e299e
rahima <abbasova.rahima.n@gmail.com>
2020-11-05 17:32:38 +0400

    fix

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3001
+app.get('/', (req,res)=> {
+    res.send('Hello World!')
+})
+app.listen(port)
+app.post('/sendmessage',(req,res)=>{
+    console.log(req)
+})
